a1565dcb348c4e11764ef6d2c960a917bf39b86d
superenginee <superengineer127@gmail.com>
2024-04-09 19:53:40 -0400

    #1113

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,6 @@
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 import { ColorModeScript } from "@chakra-ui/react";
- 
+
 export default class Document extends NextDocument {
   render() {
     return (
@@ -10,7 +10,7 @@ export default class Document extends NextDocument {
             rel="icon"
             sizes="any"
             type="image/svg+xml"
-            href="favicon.svg"
+            href="/favicon.svg"
           />
         </Head>
         <body>
